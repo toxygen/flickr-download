4a260d31b3dc86441bc8fb937e7d21254edc2a83
Marian Such <toxygen1@gmail.com>
2014-05-31 02:32:54 +0200

    new features

--- flickr_download/flick_download.py
+++ flickr_download/flick_download.py
@@ -68,6 +68,25 @@ def _load_defaults():
 
     return {}
 
+def download_list(list, directory, size_label=None):
+    for photo in list:
+        fname = '{0}.jpg'.format(photo.id)
+        if os.path.exists(fname):
+            # TODO: Ideally we should check for file size / md5 here
+            # to handle failed downloads.
+            print 'Skipping {0}, as it exists already'.format(fname)
+            continue
+		
+        print 'Saving: {0}'.format(fname)
+        photo.save(fname, size_label)
+		
+        # Set file times to when the photo was taken
+        info = photo.getInfo()
+        taken = parser.parse(info['taken'])
+        taken_unix = time.mktime(taken.timetuple())
+        os.utime(fname, (taken_unix, taken_unix))
+
+    os.chdir("..")
 
 def download_set(set_id, size_label=None):
     """
@@ -77,24 +96,8 @@ def download_set(set_id, size_label=None):
     @param size_label: str|None, size to download (or None for largest available)
     """
     pset = Flickr.Photoset(id=set_id)
-    photos = pset.getPhotos()
-    for photo in photos:
-        fname = '{0}.jpg'.format(photo.id)
-        if os.path.exists(fname):
-            # TODO: Ideally we should check for file size / md5 here
-            # to handle failed downloads.
-            print 'Skipping {0}, as it exists already'.format(fname)
-            continue
-
-        print 'Saving: {0}'.format(fname)
-        photo.save(fname, size_label)
-
-        # Set file times to when the photo was taken
-        info = photo.getInfo()
-        taken = parser.parse(info['taken'])
-        taken_unix = time.mktime(taken.timetuple())
-        os.utime(fname, (taken_unix, taken_unix))
-
+    photos = Flickr.Walker(pset.getPhotos)
+    download_list(photos, size_label)
 
 def print_sets(username):
     """
@@ -103,10 +106,49 @@ def print_sets(username):
     @param username: str,
     """
     user = Flickr.Person.findByUserName(username)
-    photosets = user.getPhotosets()
-    for photo in photosets:
-        print '{0} - {1}'.format(photo.id, photo.title)
+    photosets = Flickr.Walker(user.getPhotosets)
+    for set in photosets:
+        print u"{0} - {1}".format(set.id, set.title)
 
+def download_photos(username, size_label=None):
+    """
+		Download photos from user 'username' to the current directory.
+		
+		@param username: str, name of the user
+		@param size_label: str|None, size to download (or None for largest available)
+		"""
+
+    user = Flickr.Person.findByUserName(username)
+    photos = Flickr.Walker(user.getPhotos)
+    download_list(photos, size_label)
+
+def list_photos(username):
+    """
+		Print all photos of the given user
+		
+		@param username: str,
+		"""
+    user = Flickr.Person.findByUserName(username)
+    photos = Flickr.Walker(user.getPhotos)
+    for photo in photos:
+        print u"{0} - {1}".format(photo.id, photo.title)
+
+    print("Number of total photos: %s" % user.getPhotos().info.total)
+
+def download_sets(username, size_label=None):
+    user = Flickr.Person.findByUserName(username)
+    photosets = Flickr.Walker(user.getPhotosets)
+
+    for set in photosets:
+        if not os.path.exists(set.id):
+            os.mkdir(set.id)
+        os.chdir(set.id)
+        download_set(set.id)
+        os.chdir(os.pardir)
+
+def download_all(username, size_label=None):
+    download_photos(username)
+    download_sets(username)
 
 def main():
     parser = argparse.ArgumentParser('Download a Flickr Set')
@@ -116,10 +158,21 @@ def main():
                         help='Flickr API secret')
     parser.add_argument('-t', '--api_token', action='store_true',
 						help='Use OAuth token')
-    parser.add_argument('-l', '--list', type=str, metavar='USER',
+    parser.add_argument('-l', '--list', type=str,
                         help='List photosets for a user')
-    parser.add_argument('-d', '--download', type=str, metavar='SET_ID',
+    parser.add_argument('-p', '--photos', type=str,
+						help='List photo of a user')
+    parser.add_argument('-d', '--download', type=str,
                         help='Download the given set')
+    parser.add_argument('-u', '--photostream', type=str,
+						help='Download photostream of user')
+    parser.add_argument('-o', '--photosets', type=str,
+						help='Download all photosets of user')
+    parser.add_argument('-x', '--all', type=str,
+                        help='Download all photosets and photos of user')
+
+
+
     parser.set_defaults(**_load_defaults())
     
     args = parser.parse_args()
@@ -133,6 +186,14 @@ def main():
         print_sets(args.list)
     elif args.download:
         download_set(args.download)
+    elif args.photos:
+	    list_photos(args.photos)
+    elif args.photosets:
+        download_sets(args.photosets)
+    elif args.photostream:
+        download_photos(args.photostream)
+    elif args.all:
+        download_all(args.all)
     else:
         print >> sys.stderr, 'ERROR: Must pass either --list or --download\n'
         parser.print_help()
